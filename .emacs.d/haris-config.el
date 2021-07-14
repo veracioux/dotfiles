086357f553d8b2be28b9aa55c2b20fae94a77fee
Haris Gušić <harisgusic.dev@gmail.com>
2021-07-14 22:24:11 +0200

    Edit emacs config
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- .emacs.d/haris-config.el
+++ .emacs.d/haris-config.el
@@ -5,11 +5,11 @@
 (add-hook 'prog-mode-hook #'auto-fill-mode)
 (add-hook 'org-mode-hook 'org-fragtog-mode)
 
+;; Ivy
+(setq ivy-initial-inputs-alist ())
+
 ;; ERC
 (load (locate-user-emacs-file "ercrc.el") nil :nomessage)
-
-;; Evil
-(setq evil-undo-system 'undo-tree)
 
 ;; Evil numbers
 (define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
@@ -22,6 +22,7 @@
 (add-hook 'octave-mode-hook
   (lambda ()
     (setq comment-start "% " comment-end "")))
+(define-key evil-normal-state-map (kbd "SPC c c") 'evilnc-copy-and-comment-lines)
 
 ;; Scroll with Alt
 (define-key evil-normal-state-map (kbd "M-k")   'evil-scroll-line-up)
@@ -30,23 +31,31 @@
 (define-key evil-insert-state-map (kbd "M-j")   'evil-scroll-line-down)
 
 ;; Evil miscellanous
+;; TODO sort this out
+(define-key evil-normal-state-map (kbd "v")   	'evil-visual-char)
 (define-key evil-normal-state-map (kbd "v v")   'evil-visual-line)
 (define-key evil-normal-state-map (kbd "<tab>") 'other-window)
-(define-key evil-normal-state-map (kbd "M-/")   'evil-ex-nohighlight)
+(define-key evil-normal-state-map (kbd "M-/")   '(lambda () (interactive)
+                                                   (evil-ex-call-command "" "noh" "")))
 (global-undo-tree-mode 1)
 (evil-set-undo-system 'undo-tree)
 
 ;; Octave mode
-(defun octave-write-and-source ()
-  (interactive)
+(defun octave-write-and-source () (interactive)
   (write-file (buffer-file-name))
   (octave-source-file (buffer-file-name)))
 
-(define-key evil-normal-state-map (kbd ",ss") 'octave-write-and-source)
+(evil-define-key 'normal octave-mode-map
+  (kbd ",ss") 'octave-write-and-source)
+(evil-define-key 'normal inferior-octave-mode-map
+  (kbd ",hh") 'octave-help)
 
 ;; Inferiors in general
-(define-key comint-mode-map (kbd "C-l") 'comint-clear-buffer)
-(define-key erc-mode-map    (kbd "C-l") 'comint-clear-buffer)
+(defun comint-clear-buffer-goto () (interactive)
+  (comint-clear-buffer) (evil-goto-line))
+
+(define-key comint-mode-map (kbd "C-l") 'comint-clear-buffer-goto)
+(define-key erc-mode-map    (kbd "C-l") 'comint-clear-buffer))
 
 ;; Toggle emphasis markers
 (defun org-toggle-emphasis ()
@@ -55,7 +64,7 @@
   (if org-hide-emphasis-markers
     (set-variable 'org-hide-emphasis-markers nil)
     (set-variable 'org-hide-emphasis-markers t))
-    (org-mode-restart))
+  (org-mode-restart))
 (global-set-key (kbd "C-c e") 'org-toggle-emphasis)
 (set-variable 'org-hide-emphasis-markers t)
 
@@ -76,11 +85,11 @@
 (require 'ob-shell)
 (require 'ob-C)
 (require 'ob-python)
-(require 'ob-ipython)
+;; (require 'ob-ipython)
 (org-babel-do-load-languages 'org-babel-load-languages
   '((shell    . t)
     (C 		  . t)
-    (ipython  . t)
+    ;; (ipython  . t)
     (python   . t))
 )
 
@@ -90,6 +99,9 @@
     '((c-c++ :variables c-c++-enable-clang-support t)))
 (setq c-default-style
   "bsd" c-basic-offset 4)
+
+;; Python
+(python :variables python-backend 'lsp python-lsp-server 'pylsp)
 
 ;; Show snippets in auto-completion popup
 (setq-default dotspacemacs-configuration-layers
