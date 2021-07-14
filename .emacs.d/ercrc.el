dd7f9e0c33d780cc4f3537d0a03044420f4e7de8
Haris Gušić <harisgusic.dev@gmail.com>
2021-07-14 23:08:31 +0200

    Add erc config file
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

---
+++ .emacs.d/ercrc.el
@@ -0,0 +1,14 @@
+; (erc :server "irc.libera.chat" :port "6697" :nick "veracioux")
+
+(setq erc-server "irc.libera.chat"
+      erc-nick "veracioux"
+      erc-user-full-name "Haris Gušić"
+      erc-track-shorten-start 8
+      erc-autojoin-channels-alist '((".*" "#archlinux" "#octave"))
+      erc-kill-buffer-on-part t
+      erc-auto-query 'bury)
+
+(add-to-list 'erc-modules 'notifications)
+(delete 'readonly erc-modules)
+(erc-services-mode 1)
+(erc-update-modules)
