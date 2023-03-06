568b748a149ba47d144c985f91d7cda058e1cad3
Haris Gušić <harisgusic.dev@gmail.com>
2023-03-06 21:22:33 +0100

    Move fish configs to shells.org

--- .config/fish/functions/enved.fish
+++
@@ -1,7 +0,0 @@
-function enved --argument envvar
-    set tmp (mktemp /tmp/envvar-XXXXXXXXX)
-    bass echo \$"$envvar" > "$tmp"
-    "$EDITOR" "$tmp"
-    bass export "$envvar"=(cat "$tmp")
-    /usr/bin/rm -f "$tmp"
-end
