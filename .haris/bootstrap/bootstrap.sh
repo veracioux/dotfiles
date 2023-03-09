afa227a48ed995047184dcf169ca04a7df37959c
Haris Gušić <harisgusic.dev@gmail.com>
2023-03-09 20:46:30 +0100

    Update bootstrap.sh with dependency installation

--- .haris/bootstrap/bootstrap.sh
+++ .haris/bootstrap/bootstrap.sh
@@ -1,21 +1,50 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
-tmpfile="$(mktemp XXXXXXXX.tar.gz)"
-extractdir="$(mktemp)"
-backupdir="$(mktemp -d)"
+tmpfile="$(mktemp -t XXXXXXXX.tar.gz)"
+extractdir="$(mktemp -d)"
+backupdir="$(mktemp -dt tangle-home-backup-"$(whoami)"-XXXXXXX)"
+
+check_missing_then_install() {
+    local distro="$(lsb_release --id | cut -f2)"
+    local install_cmd
+    local packages=()
+    case "$distro" in
+        Arch)
+            install_cmd=(sudo pacman -S);;
+        Ubuntu)
+            install_cmd=(sudo apt install);;
+    esac
+    for name in "$@"; do
+        if [ ! -x "$(command -v "$name")" ]; then
+            packages=("${packages[@]}" "$name")
+        fi
+    done
+    if [ "${#packages[@]}" = 0 ]; then
+        return 0;
+    fi
+    echo "Some required commands missing. Will be installed using:" >&2
+    echo "    ${install_cmd[*]} ${packages[@]}" >&2
+    read -n1 -p "Ok? [Y/n]: " ok
+    if [ -z "$ok" -o "${ok^}" = "Y" ]; then
+        "${install_cmd[@]}" "${packages[@]}"
+    else
+        return 1
+    fi
+}
+
+check_missing_then_install docker rsync
 
 echo "--------------------------------"
 echo "Generating configs into tar file"
 echo "--------------------------------"
-docker run docker.veracioux.me/dotfiles-generator >"$tmpfile" --user "$(whoami)" --home ~
+docker run docker.veracioux.me/dotfiles-generator >"$tmpfile" --user "$(whoami)" --home ~ --branch bootstrap # TODO tmp
 
 echo "--------------------------------"
 echo "Extracting tar file"
 echo "--------------------------------"
-mkdir "$extractdir"
-tar xf "$tmpfile" -C
+tar xf "$tmpfile" --directory "$extractdir"
 
 echo "--------------------------------"
 echo "Copying to home directory"
@@ -25,4 +54,4 @@ rsync -rv --backup-dir "$backupdir" "$extractdir/" ~/
 echo "--------------------------------"
 echo "Overwritten files backed up to $backupdir"
 echo "In case of error, restore them using:"
-echo "  rsync -rv $backupdir"
+echo "  rsync -rv $backupdir/ ~/"
