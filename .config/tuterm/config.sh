203b34b78dcbc2cd7bef781e2b7d466abd370a3b
Haris Gušić <harisgusic.dev@gmail.com>
2023-03-12 10:11:52 +0100

    Move all configs to org files and other improvements

--- .config/tuterm/config.sh
+++
@@ -1,5 +0,0 @@
-TUTERM_SHELL='fish'
-
-prompt() {
-    echo -ne "\033[1;94m$TUTERM_NAME \033[1;35m$(pwd | sed "s:$HOME:~:")\033[0;33m $\033[0m "
-}
--- .startup.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import subprocess as sp
-import importlib as imp
-import re
-from importlib import reload
-
-pwd = os.getcwd
-ls = lambda path=".": print(sp.run(["ls", *os.listdir(path)]).stdout)
