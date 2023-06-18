033bb8e3582224078ec87c571669ca9af5f14686
Haris Gušić <harisgusic.dev@gmail.com>
2023-06-18 23:06:32 +0200

    bootstrap: Fix error and use local files instead of cloning

--- bootstrap/entrypoint.sh
+++ bootstrap/entrypoint.sh
@@ -8,31 +8,19 @@ set -e
 
 usage() {
     echo 'Usage: docker run docker.veracioux.me/dotfiles-generator [OPTIONS]'
-    echo '  -r, --remote    The remote repository to clone'
-    echo '  -b, --branch    The git ref to checkout'
     echo '  -u, --user      The UNIX user the config is meant for'
     echo '  -h, --home      The path of the home directory, if it differs from /home/<user>'
 }
 
-parsed_args="$(getopt --name run --options hr:b:u:h --long help,remote:,branch:,user:,home: -- "$@")"
+parsed_args="$(getopt --name run --options hr:b:u:h --long help,user:,home: -- "$@")"
 
 eval set -- "$parsed_args"
 
-remote=https://github.com/veracioux/dotfiles
-branch=master
 home=''
 user=''
 
 while :; do
     case "$1" in
-        -r | --remote)
-            remote="$2"
-            shift 2
-            ;;
-        -b | --branch)
-            branch="$2"
-            shift 2
-            ;;
         -u | --user)
             user="$2"
             if [ -z "$home" ]; then
@@ -59,17 +47,11 @@ if [ -z "$user" ]; then
     exit 1
 fi
 
-mkdir -p ~/.haris
 cd ~/.haris
-git config --global init.defaultBranch non-master-to-avoid-conflict
-git init
-git remote add origin "$remote"
-git fetch origin "$branch:$branch" --depth 1
-git checkout "$branch"
 
 # These are used by the tangle script to determine destinations
 export USER="$user" HOME="$home"
-emacs --script .haris/bootstrap/tangle-all.sh
+emacs --script bootstrap/tangle-all.sh
 
 exec 1>&3
 cd /tmp/tangle-"$user"
