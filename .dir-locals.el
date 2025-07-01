4ec8986fa079d6ff929707e59dd894bec095ced4
Haris Gušić <harisgusic.dev@gmail.com>
2025-07-01 11:21:20 +0200

    feat: emacs: Enhance commit prompts with detailed guidance

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((haris/allow-copilot . t))))
+((nil . ((haris/allow-copilot . t)))
+ (magit-status-mode . ((eval . (setq-local magit-gptcommit-prompt haris/commit-prompt-dotfiles)))))
