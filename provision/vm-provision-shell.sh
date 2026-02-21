11247848b8729d7d8a2472a6250a27588563a765
Haris Gušić <harisgusic.dev@gmail.com>
2026-02-21 15:29:31 +0100

    fix: provision: Check authorized_keys existence before grep

--- provision/vm-provision-shell.sh
+++ provision/vm-provision-shell.sh
@@ -2,14 +2,14 @@
 
 set -e
 
+echo haris | /vagrant/bootstrap-ansible.sh haris
+
 mkdir -p ~haris/.ssh
 
 while read pubkey; do
-    if ! grep -q "$pubkey" ~haris/.ssh/authorized_keys; then
+    if [ ! -e ~haris/.ssh/authorized_keys ] || ! grep -q "$pubkey" ~haris/.ssh/authorized_keys; then
         echo "$pubkey" >> ~haris/.ssh/authorized_keys
     fi
 done </vagrant/vm_authorized_keys.pub
 
 chown -R haris:haris ~haris/.ssh
-
-echo haris | /vagrant/bootstrap-ansible.sh haris
